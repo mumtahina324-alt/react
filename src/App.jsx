7d9c977667e5e174eefab0c5cce33a67e88c3a4d
mumtahina324-alt <mumtahina324@gmail.com>
2025-12-27 18:22:14 +0600

    Initilize react app & implement tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+export default function App() {
+  // ok amader react app create & tailwind css install hoye geche, akhn amra github a jeye ekta repo create korbo, amra sob somoi app ar nam jeita repo nam o seitai rakhar chesta korbo, ekhane app ar nam react
+  return <div>App</div>;
+}
